d5cf5b47e6f647ce12a4ec698b7da4cdc497c883
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-29 14:13:30 +0300

    initial separation from the other repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# JS Twitter Bot
+
+## Next Step
+
+move timestamps and little config to firebase
+
+> Doesn't work after twitter API monetiziation
---
+++ rebuild_docker.sh
@@ -0,0 +1,6 @@
+git pull
+docker stop js_twitter_bot
+docker rm js_twitter_bot
+docker rmi mo9a7i/js_twitter_bot
+docker build -t mo9a7i/js_twitter_bot .
+docker run --name js_twitter_bot -d mo9a7i/js_twitter_bot
